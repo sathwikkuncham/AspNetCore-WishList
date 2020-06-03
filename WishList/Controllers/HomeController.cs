1596412f8f8365172471e99648caba2ae3f68af0
sathwik kuncham <sathwikkuncham@gmail.com>
2020-06-04 02:49:55 +0530

    Error fixes

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -10,11 +10,11 @@ namespace WishList.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
         public IActionResult Error()
         {
-            return View();
+            return View("Error");
         }
     }
 }
