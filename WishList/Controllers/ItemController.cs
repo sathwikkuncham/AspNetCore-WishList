238daca5e937ce607671327c2c273b3719d0c73a
sathwik kuncham <sathwikkuncham@gmail.com>
2020-06-04 14:29:10 +0530

    Final module

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+        public IActionResult Index()
+        {
+            return View("Index",_context.Items);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        [HttpPost]
+        public IActionResult PostCreate(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
